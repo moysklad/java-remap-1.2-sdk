bd9410a4f579638ed974132b8c25705e19fce0bd
eevtuhova <eevtuhova@moysklad.ru>
2017-09-12 11:05:43 +0300

    add test testReadPaymentsInWithAgentAccountExpand

--- src/test/java/com/lognex/api/DocumentEndpointTest.java
+++ src/test/java/com/lognex/api/DocumentEndpointTest.java
@@ -43,4 +43,17 @@ public class DocumentEndpointTest {
         assertEquals(new ID("ac08418c-9482-11e7-7a69-8f550003b1e0"), paymentIn.getId());
         assertEquals(new ID("81c97f7f-9482-11e7-7a6c-d2a9000847cd"), paymentIn.getAgentAccount().getId());
     }
+
+    @Test
+    public void testReadPaymentsInWithAgentAccountExpand() throws ConverterException {
+        API api = new API();
+        API.RequestBuilder requestBuilder = api.initRequest(System.getenv("login"), System.getenv("password"))
+                .expand("agentAccount");
+        List<PaymentIn> paymentIns = documentEndpoint.readPaymentInList(requestBuilder);
+        assertFalse(paymentIns.stream()
+                .anyMatch(p -> p.getId() == null));
+        assertFalse(paymentIns.stream()
+                .map(PaymentIn::getAgentAccount)
+                .anyMatch(a -> a.getId() == null));
+    }
 }
