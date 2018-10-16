11efb2537234737194172b3f5648849cd9f51bb5
PepRoll <peproll@protonmail.com>
2018-10-16 11:41:41 +0300

    MC-28321 fix entities updating. query param is added to the list endpoint

--- src/main/java/com/lognex/api/clients/ProductFolderClient.java
+++ src/main/java/com/lognex/api/clients/ProductFolderClient.java
@@ -1,15 +1,19 @@
 package com.lognex.api.clients;
 
 import com.lognex.api.LognexApi;
+import com.lognex.api.clients.endpoints.DeleteByIdEndpoint;
 import com.lognex.api.clients.endpoints.GetListEndpoint;
 import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.PutEndpoint;
 import com.lognex.api.entities.ProductFolderEntity;
 
 public final class ProductFolderClient
         extends ApiClient
         implements
         GetListEndpoint<ProductFolderEntity>,
-        PostEndpoint<ProductFolderEntity> {
+        PostEndpoint<ProductFolderEntity>,
+        PutEndpoint<ProductFolderEntity>,
+        DeleteByIdEndpoint {
 
     public ProductFolderClient(LognexApi api) {
         super(api, "/entity/productfolder/", ProductFolderEntity.class);
--- src/main/java/com/lognex/api/clients/ServiceClient.java
+++ src/main/java/com/lognex/api/clients/ServiceClient.java
@@ -1,15 +1,19 @@
 package com.lognex.api.clients;
 
 import com.lognex.api.LognexApi;
+import com.lognex.api.clients.endpoints.DeleteByIdEndpoint;
 import com.lognex.api.clients.endpoints.GetListEndpoint;
 import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.PutEndpoint;
 import com.lognex.api.entities.products.ServiceEntity;
 
 public final class ServiceClient
         extends ApiClient
         implements
         GetListEndpoint<ServiceEntity>,
-        PostEndpoint<ServiceEntity> {
+        PostEndpoint<ServiceEntity>,
+        PutEndpoint<ServiceEntity>,
+        DeleteByIdEndpoint {
 
     public ServiceClient(LognexApi api) {
         super(api, "/entity/service/", ServiceEntity.class);
