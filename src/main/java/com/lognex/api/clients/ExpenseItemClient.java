8eb54e4f0fb31788587b7f452d93887835bdab5d
ereztsov <ereztsov@moysklad.ru>
2019-05-22 17:03:43 +0300

    MC-31037 Fixed tests В клиенте ExpenseItem добавлены эндпоинты deleteById, getById, putById Добавлены тесты

--- src/main/java/com/lognex/api/clients/ExpenseItemClient.java
+++ src/main/java/com/lognex/api/clients/ExpenseItemClient.java
@@ -1,8 +1,7 @@
 package com.lognex.api.clients;
 
 import com.lognex.api.LognexApi;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.ExpenseItemEntity;
 import com.lognex.api.entities.MetaEntity;
 
@@ -10,7 +9,10 @@ public final class ExpenseItemClient
         extends ApiClient
         implements
         GetListEndpoint<ExpenseItemEntity>,
-        PostEndpoint<ExpenseItemEntity> {
+        PostEndpoint<ExpenseItemEntity>,
+        DeleteByIdEndpoint,
+        GetByIdEndpoint<ExpenseItemEntity>,
+        PutByIdEndpoint<ExpenseItemEntity> {
 
     public ExpenseItemClient(LognexApi api) {
         super(api, "/entity/expenseitem/");
---
+++ src/test/java/com/lognex/api/entities/ExpenseItemEntityTest.java
@@ -0,0 +1,123 @@
+package com.lognex.api.entities;
+
+import com.lognex.api.responses.ListEntity;
+import com.lognex.api.utils.LognexApiException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static com.lognex.api.utils.params.FilterParam.filterEq;
+import static org.junit.Assert.*;
+
+public class ExpenseItemEntityTest extends EntityTestBase {
+    @Test
+    public void createTest() throws IOException, LognexApiException {
+        ExpenseItemEntity e = new ExpenseItemEntity();
+        e.setName("expenseitem_" + randomString(3) + "_" + new Date().getTime());
+        e.setDescription(randomString());
+        e.setCode(randomString());
+        e.setExternalCode(randomString());
+
+        api.entity().expenseitem().post(e);
+
+        ListEntity<ExpenseItemEntity> updatedEntitiesList = api.entity().expenseitem().get(filterEq("name", e.getName()));
+        assertEquals(1, updatedEntitiesList.getRows().size());
+
+        ExpenseItemEntity retrievedEntity = updatedEntitiesList.getRows().get(0);
+        assertEquals(e.getName(), retrievedEntity.getName());
+        assertEquals(e.getDescription(), retrievedEntity.getDescription());
+        assertEquals(e.getCode(), retrievedEntity.getCode());
+        assertEquals(e.getExternalCode(), retrievedEntity.getExternalCode());
+        assertEquals(e.getUpdated().withNano(0), retrievedEntity.getUpdated().withNano(0));
+    }
+
+    @Test
+    public void getTest() throws IOException, LognexApiException {
+        ExpenseItemEntity e = createSimpleExpenseItem();
+
+        ExpenseItemEntity retrievedEntity = api.entity().expenseitem().get(e.getId());
+        getAsserts(e, retrievedEntity);
+
+        retrievedEntity = api.entity().expenseitem().get(e);
+        getAsserts(e, retrievedEntity);
+    }
+
+    @Test
+    public void putTest() throws IOException, LognexApiException, InterruptedException {
+        ExpenseItemEntity e = createSimpleExpenseItem();
+
+        ExpenseItemEntity retrievedOriginalEntity = api.entity().expenseitem().get(e.getId());
+        String name = "expenseitem_" + randomString(3) + "_" + new Date().getTime();
+        e.setName(name);
+        Thread.sleep(1500);
+        api.entity().expenseitem().put(e.getId(), e);
+        putAsserts(e, retrievedOriginalEntity, name);
+
+        e = createSimpleExpenseItem();
+        retrievedOriginalEntity = api.entity().expenseitem().get(e.getId());
+        name = "expenseitem_" + randomString(3) + "_" + new Date().getTime();
+        e.setName(name);
+        Thread.sleep(1500);
+        api.entity().expenseitem().put(e);
+        putAsserts(e, retrievedOriginalEntity, name);
+    }
+
+    @Test
+    public void deleteTest() throws IOException, LognexApiException {
+        ExpenseItemEntity e = createSimpleExpenseItem();
+
+        ListEntity<ExpenseItemEntity> entitiesList = api.entity().expenseitem().get(filterEq("name", e.getName()));
+        assertEquals(1, entitiesList.getRows().size());
+
+        api.entity().expenseitem().delete(e.getId());
+
+        entitiesList = api.entity().expenseitem().get(filterEq("name", e.getName()));
+        assertEquals(0, entitiesList.getRows().size());
+    }
+
+    @Test
+    public void deleteByIdTest() throws IOException, LognexApiException {
+        ExpenseItemEntity e = createSimpleExpenseItem();
+
+        ListEntity<ExpenseItemEntity> entitiesList = api.entity().expenseitem().get(filterEq("name", e.getName()));
+        assertEquals(1, entitiesList.getRows().size());
+
+        api.entity().expenseitem().delete(e);
+
+        entitiesList = api.entity().expenseitem().get(filterEq("name", e.getName()));
+        assertEquals(0, entitiesList.getRows().size());
+    }
+
+    private ExpenseItemEntity createSimpleExpenseItem() throws IOException, LognexApiException {
+        ExpenseItemEntity e = new ExpenseItemEntity();
+        e.setName("expenseitem_" + randomString(3) + "_" + new Date().getTime());
+        e.setDescription(randomString());
+        e.setCode(randomString());
+        e.setExternalCode(randomString());
+
+        api.entity().expenseitem().post(e);
+
+        return e;
+    }
+
+    private void getAsserts(ExpenseItemEntity e, ExpenseItemEntity retrievedEntity) {
+        assertEquals(e.getName(), retrievedEntity.getName());
+        assertEquals(e.getDescription(), retrievedEntity.getDescription());
+        assertEquals(e.getCode(), retrievedEntity.getCode());
+        assertEquals(e.getExternalCode(), retrievedEntity.getExternalCode());
+        assertEquals(e.getUpdated().withNano(0), retrievedEntity.getUpdated().withNano(0));
+    }
+
+    private void putAsserts(ExpenseItemEntity e, ExpenseItemEntity retrievedOriginalEntity, String name) throws IOException, LognexApiException {
+        ExpenseItemEntity retrievedUpdatedEntity = api.entity().expenseitem().get(e.getId());
+
+        assertNotEquals(retrievedOriginalEntity.getName(), retrievedUpdatedEntity.getName());
+        assertEquals(name, retrievedUpdatedEntity.getName());
+        assertEquals(retrievedOriginalEntity.getDescription(), retrievedUpdatedEntity.getDescription());
+        assertEquals(retrievedOriginalEntity.getCode(), retrievedUpdatedEntity.getCode());
+        assertEquals(retrievedOriginalEntity.getExternalCode(), retrievedUpdatedEntity.getExternalCode());
+//        assertNotEquals(retrievedOriginalEntity.getUpdated(), retrievedUpdatedEntity.getUpdated());
+    }
+}
+
