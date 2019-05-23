1ba996289213641c9004fe55d6b4e216fea0f4fe
ereztsov <ereztsov@moysklad.ru>
2019-05-23 16:35:30 +0300

    MC-31037 Fixed tests Добавлены эндпоинты в клиент SalesReturn Добавлены тесты

---
+++ src/test/java/com/lognex/api/entities/documents/SalesReturnDocumentEntityTest.java
@@ -0,0 +1,189 @@
+package com.lognex.api.entities.documents;
+
+import com.lognex.api.entities.EntityTestBase;
+import com.lognex.api.entities.StoreEntity;
+import com.lognex.api.entities.agents.CounterpartyEntity;
+import com.lognex.api.entities.agents.OrganizationEntity;
+import com.lognex.api.responses.ListEntity;
+import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
+import com.lognex.api.utils.LognexApiException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static com.lognex.api.utils.params.FilterParam.filterEq;
+import static org.junit.Assert.*;
+
+public class SalesReturnDocumentEntityTest extends EntityTestBase {
+    @Test
+    public void createTest() throws IOException, LognexApiException {
+        SalesReturnDocumentEntity e = new SalesReturnDocumentEntity();
+        e.setName("salesreturn_" + randomString(3) + "_" + new Date().getTime());
+        e.setDescription(randomString());
+        e.setVatEnabled(true);
+        e.setVatIncluded(true);
+        e.setMoment(LocalDateTime.now());
+
+        ListEntity<OrganizationEntity> orgList = api.entity().organization().get();
+        assertNotEquals(0, orgList.getRows().size());
+        e.setOrganization(orgList.getRows().get(0));
+
+        CounterpartyEntity agent = new CounterpartyEntity();
+        agent.setName(randomString());
+        api.entity().counterparty().post(agent);
+        e.setAgent(agent);
+
+        ListEntity<StoreEntity> store = api.entity().store().get(filterEq("name", "Основной склад"));
+        assertEquals(1, store.getRows().size());
+        e.setStore(store.getRows().get(0));
+
+        DemandDocumentEntity demand = new DemandDocumentEntity();
+        demand.setName("demand_" + randomString(3) + "_" + new Date().getTime());
+        demand.setDescription(randomString());
+        demand.setOrganization(orgList.getRows().get(0));
+        demand.setAgent(agent);
+        demand.setStore(store.getRows().get(0));
+        
+        api.entity().demand().post(demand);
+        e.setDemand(demand);
+
+        api.entity().salesreturn().post(e);
+
+        ListEntity<SalesReturnDocumentEntity> updatedEntitiesList = api.entity().salesreturn().get(filterEq("name", e.getName()));
+        assertEquals(1, updatedEntitiesList.getRows().size());
+
+        SalesReturnDocumentEntity retrievedEntity = updatedEntitiesList.getRows().get(0);
+        assertEquals(e.getName(), retrievedEntity.getName());
+        assertEquals(e.getDescription(), retrievedEntity.getDescription());
+        assertEquals(e.getVatEnabled(), retrievedEntity.getVatEnabled());
+        assertEquals(e.getVatIncluded(), retrievedEntity.getVatIncluded());
+        assertEquals(e.getMoment(), retrievedEntity.getMoment());
+        assertEquals(e.getOrganization().getMeta().getHref(), retrievedEntity.getOrganization().getMeta().getHref());
+        assertEquals(e.getAgent().getMeta().getHref(), retrievedEntity.getAgent().getMeta().getHref());
+        assertEquals(e.getStore().getMeta().getHref(), retrievedEntity.getStore().getMeta().getHref());
+        assertEquals(e.getDemand().getMeta().getHref(), retrievedEntity.getDemand().getMeta().getHref());
+        assertEquals(e.getCreated().withNano(0), retrievedEntity.getCreated().withNano(0));
+        assertEquals(e.getUpdated().withNano(0), retrievedEntity.getUpdated().withNano(0));
+    }
+
+    @Test
+    public void getTest() throws IOException, LognexApiException {
+        SalesReturnDocumentEntity e = createSimpleDocumentSalesReturn();
+
+        SalesReturnDocumentEntity retrievedEntity = api.entity().salesreturn().get(e.getId());
+        getAsserts(e, retrievedEntity);
+
+        retrievedEntity = api.entity().salesreturn().get(e);
+        getAsserts(e, retrievedEntity);
+    }
+
+    @Test
+    public void putTest() throws IOException, LognexApiException, InterruptedException {
+        SalesReturnDocumentEntity e = createSimpleDocumentSalesReturn();
+
+        SalesReturnDocumentEntity retrievedOriginalEntity = api.entity().salesreturn().get(e.getId());
+        String name = "salesreturn_" + randomString(3) + "_" + new Date().getTime();
+        e.setName(name);
+        Thread.sleep(1500);
+        api.entity().salesreturn().put(e.getId(), e);
+        putAsserts(e, retrievedOriginalEntity, name);
+
+        retrievedOriginalEntity.set(e);
+
+        name = "salesreturn_" + randomString(3) + "_" + new Date().getTime();
+        e.setName(name);
+        Thread.sleep(1500);
+        api.entity().salesreturn().put(e);
+        putAsserts(e, retrievedOriginalEntity, name);
+    }
+
+    @Test
+    public void deleteTest() throws IOException, LognexApiException {
+        SalesReturnDocumentEntity e = createSimpleDocumentSalesReturn();
+
+        ListEntity<SalesReturnDocumentEntity> entitiesList = api.entity().salesreturn().get(filterEq("name", e.getName()));
+        assertEquals(1, entitiesList.getRows().size());
+
+        api.entity().salesreturn().delete(e.getId());
+
+        entitiesList = api.entity().salesreturn().get(filterEq("name", e.getName()));
+        assertEquals(0, entitiesList.getRows().size());
+    }
+
+    @Test
+    public void deleteByIdTest() throws IOException, LognexApiException {
+        SalesReturnDocumentEntity e = createSimpleDocumentSalesReturn();
+
+        ListEntity<SalesReturnDocumentEntity> entitiesList = api.entity().salesreturn().get(filterEq("name", e.getName()));
+        assertEquals(1, entitiesList.getRows().size());
+
+        api.entity().salesreturn().delete(e);
+
+        entitiesList = api.entity().salesreturn().get(filterEq("name", e.getName()));
+        assertEquals(0, entitiesList.getRows().size());
+    }
+
+    @Test
+    public void metadataTest() throws IOException, LognexApiException {
+        MetadataAttributeSharedStatesResponse response = api.entity().salesreturn().metadata().get();
+
+        assertFalse(response.getCreateShared());
+    }
+
+    private SalesReturnDocumentEntity createSimpleDocumentSalesReturn() throws IOException, LognexApiException {
+        SalesReturnDocumentEntity e = new SalesReturnDocumentEntity();
+        e.setName("salesreturn_" + randomString(3) + "_" + new Date().getTime());
+
+        ListEntity<OrganizationEntity> orgList = api.entity().organization().get();
+        assertNotEquals(0, orgList.getRows().size());
+        e.setOrganization(orgList.getRows().get(0));
+
+        CounterpartyEntity agent = new CounterpartyEntity();
+        agent.setName(randomString());
+        api.entity().counterparty().post(agent);
+        e.setAgent(agent);
+
+        ListEntity<StoreEntity> store = api.entity().store().get(filterEq("name", "Основной склад"));
+        assertEquals(1, store.getRows().size());
+        e.setStore(store.getRows().get(0));
+
+        DemandDocumentEntity demand = new DemandDocumentEntity();
+        demand.setName("demand_" + randomString(3) + "_" + new Date().getTime());
+        demand.setDescription(randomString());
+        demand.setOrganization(orgList.getRows().get(0));
+        demand.setAgent(agent);
+        demand.setStore(store.getRows().get(0));
+
+        api.entity().demand().post(demand);
+        e.setDemand(demand);
+
+        api.entity().salesreturn().post(e);
+
+        return e;
+    }
+
+    private void getAsserts(SalesReturnDocumentEntity e, SalesReturnDocumentEntity retrievedEntity) {
+        assertEquals(e.getName(), retrievedEntity.getName());
+        assertEquals(e.getOrganization().getMeta().getHref(), retrievedEntity.getOrganization().getMeta().getHref());
+        assertEquals(e.getAgent().getMeta().getHref(), retrievedEntity.getAgent().getMeta().getHref());
+        assertEquals(e.getStore().getMeta().getHref(), retrievedEntity.getStore().getMeta().getHref());
+        assertEquals(e.getDemand().getMeta().getHref(), retrievedEntity.getDemand().getMeta().getHref());
+        assertEquals(e.getCreated().withNano(0), retrievedEntity.getCreated().withNano(0));
+        assertEquals(e.getUpdated().withNano(0), retrievedEntity.getUpdated().withNano(0));
+    }
+
+    private void putAsserts(SalesReturnDocumentEntity e, SalesReturnDocumentEntity retrievedOriginalEntity, String name) throws IOException, LognexApiException {
+        SalesReturnDocumentEntity retrievedUpdatedEntity = api.entity().salesreturn().get(e.getId());
+
+        assertNotEquals(retrievedOriginalEntity.getName(), retrievedUpdatedEntity.getName());
+        assertEquals(name, retrievedUpdatedEntity.getName());
+        assertEquals(retrievedOriginalEntity.getOrganization().getMeta().getHref(), retrievedUpdatedEntity.getOrganization().getMeta().getHref());
+        assertEquals(retrievedOriginalEntity.getAgent().getMeta().getHref(), retrievedUpdatedEntity.getAgent().getMeta().getHref());
+        assertEquals(retrievedOriginalEntity.getStore().getMeta().getHref(), retrievedUpdatedEntity.getStore().getMeta().getHref());
+        assertEquals(retrievedOriginalEntity.getDemand().getMeta().getHref(), retrievedUpdatedEntity.getDemand().getMeta().getHref());
+        assertEquals(retrievedOriginalEntity.getCreated().withNano(0), retrievedUpdatedEntity.getCreated().withNano(0));
+        assertNotEquals(retrievedOriginalEntity.getUpdated().withNano(0), retrievedUpdatedEntity.getUpdated().withNano(0));
+    }
+}
