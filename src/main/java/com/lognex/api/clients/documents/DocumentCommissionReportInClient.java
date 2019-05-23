175bf743aad037df586f533ee4271da537ee38d8
ereztsov <ereztsov@moysklad.ru>
2019-05-23 17:36:31 +0300

    MC-30557 Fixed tests Добавлены эндпоинты в клиент ComissionReportIn Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentCommissionReportInClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentCommissionReportInClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.CommissionReportInDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentCommissionReportInClient
         implements
         GetListEndpoint<CommissionReportInDocumentEntity>,
         PostEndpoint<CommissionReportInDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<CommissionReportInDocumentEntity>,
+        PutByIdEndpoint<CommissionReportInDocumentEntity> {
 
     public DocumentCommissionReportInClient(LognexApi api) {
         super(api, "/entity/commissionreportin/");
