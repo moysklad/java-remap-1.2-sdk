11e049e058bfe3fbee102321523b5c3b64620e35
ereztsov <ereztsov@moysklad.ru>
2019-05-23 16:47:53 +0300

    MC-30557 Fixed tests Добавлены эндпоинты в клиент Supply Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentSupplyClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentSupplyClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.SupplyDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentSupplyClient
         implements
         GetListEndpoint<SupplyDocumentEntity>,
         PostEndpoint<SupplyDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<SupplyDocumentEntity>,
+        PutByIdEndpoint<SupplyDocumentEntity> {
 
     public DocumentSupplyClient(LognexApi api) {
         super(api, "/entity/supply/");
