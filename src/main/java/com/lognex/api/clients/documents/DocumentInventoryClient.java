e8599c0fdc337ed825b7da1a44eeddb8dd712453
ereztsov <ereztsov@moysklad.ru>
2019-05-23 17:59:55 +0300

    MC-31037 Fixed tests Добавлены эндпоинты в клиент Inventory Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentInventoryClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentInventoryClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.InventoryDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentInventoryClient
         implements
         GetListEndpoint<InventoryDocumentEntity>,
         PostEndpoint<InventoryDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<InventoryDocumentEntity>,
+        PutByIdEndpoint<InventoryDocumentEntity> {
 
     public DocumentInventoryClient(LognexApi api) {
         super(api, "/entity/inventory/");
