2a73c803a8f40c4c9cec59f27b958a587dd0ee6f
ereztsov <ereztsov@moysklad.ru>
2019-05-23 18:09:35 +0300

    MC-31037 Fixed tests Добавлены эндпоинты в клиент PaymentOut Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentPaymentOutClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentPaymentOutClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.PaymentOutDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentPaymentOutClient
         implements
         GetListEndpoint<PaymentOutDocumentEntity>,
         PostEndpoint<PaymentOutDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<PaymentOutDocumentEntity>,
+        PutByIdEndpoint<PaymentOutDocumentEntity> {
 
     public DocumentPaymentOutClient(LognexApi api) {
         super(api, "/entity/paymentout/");
