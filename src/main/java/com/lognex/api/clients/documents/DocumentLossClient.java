c0c33a1f6af0b7fe44399e741a77a9f5856f86e8
ereztsov <ereztsov@moysklad.ru>
2019-05-23 13:33:56 +0300

    MC-31037 Fixed tests Добавлены эндпоинты в клиент Loss Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentLossClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentLossClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.LossDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,12 @@ public final class DocumentLossClient
         implements
         GetListEndpoint<LossDocumentEntity>,
         PostEndpoint<LossDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        DocumentNewEndpoint<LossDocumentEntity>,
+        GetByIdEndpoint<LossDocumentEntity>,
+        PutByIdEndpoint<LossDocumentEntity> {
 
     public DocumentLossClient(LognexApi api) {
         super(api, "/entity/loss/");
