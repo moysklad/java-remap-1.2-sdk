f9fca43e7e1d823567c35aafb2ced28a5b2df126
ereztsov <ereztsov@moysklad.ru>
2019-05-23 16:35:30 +0300

    MC-30557 Fixed tests Добавлены эндпоинты в клиент SalesReturn Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentSalesReturnClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentSalesReturnClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.SalesReturnDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentSalesReturnClient
         implements
         GetListEndpoint<SalesReturnDocumentEntity>,
         PostEndpoint<SalesReturnDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<SalesReturnDocumentEntity>,
+        PutByIdEndpoint<SalesReturnDocumentEntity> {
 
     public DocumentSalesReturnClient(LognexApi api) {
         super(api, "/entity/salesreturn/");
