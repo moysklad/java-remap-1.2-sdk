26e0996d2a5d513a6ed4db892181564f436836c7
ereztsov <ereztsov@moysklad.ru>
2019-05-24 10:58:25 +0300

    MC-30557 Fixed tests Добавлены эндпоинты в клиент Pricelist Изменен тип поля priceType документа Pricelist Добавлены тесты

--- src/main/java/com/lognex/api/clients/documents/DocumentPricelistClient.java
+++ src/main/java/com/lognex/api/clients/documents/DocumentPricelistClient.java
@@ -2,10 +2,7 @@ package com.lognex.api.clients.documents;
 
 import com.lognex.api.LognexApi;
 import com.lognex.api.clients.ApiClient;
-import com.lognex.api.clients.endpoints.DocumentMetadataEndpoint;
-import com.lognex.api.clients.endpoints.GetListEndpoint;
-import com.lognex.api.clients.endpoints.MetadataAttributeEndpoint;
-import com.lognex.api.clients.endpoints.PostEndpoint;
+import com.lognex.api.clients.endpoints.*;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.PricelistDocumentEntity;
 import com.lognex.api.responses.metadata.MetadataAttributeSharedStatesResponse;
@@ -15,8 +12,11 @@ public final class DocumentPricelistClient
         implements
         GetListEndpoint<PricelistDocumentEntity>,
         PostEndpoint<PricelistDocumentEntity>,
+        DeleteByIdEndpoint,
         DocumentMetadataEndpoint<MetadataAttributeSharedStatesResponse>,
-        MetadataAttributeEndpoint {
+        MetadataAttributeEndpoint,
+        GetByIdEndpoint<PricelistDocumentEntity>,
+        PutByIdEndpoint<PricelistDocumentEntity> {
 
     public DocumentPricelistClient(LognexApi api) {
         super(api, "/entity/pricelist/");
