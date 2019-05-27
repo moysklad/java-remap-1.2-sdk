7fa2649a7acc4a48e1d5c9c63f23e5beeb60c01c
ereztsov <ereztsov@moysklad.ru>
2019-05-27 14:32:47 +0300

    MC-30557 Fixed tests Добавлен эндпоинт /new для Supply Добавлены тесты

--- src/test/java/com/lognex/api/entities/documents/SupplyDocumentEntityTest.java
+++ src/test/java/com/lognex/api/entities/documents/SupplyDocumentEntityTest.java
@@ -1,6 +1,7 @@
 package com.lognex.api.entities.documents;
 
 import com.lognex.api.entities.EntityTestBase;
+import com.lognex.api.entities.GroupEntity;
 import com.lognex.api.entities.StoreEntity;
 import com.lognex.api.entities.agents.CounterpartyEntity;
 import com.lognex.api.entities.agents.OrganizationEntity;
@@ -11,9 +12,13 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.Optional;
 
 import static com.lognex.api.utils.params.FilterParam.filterEq;
+import static com.lognex.api.utils.params.SearchParam.search;
 import static org.junit.Assert.*;
 
 public class SupplyDocumentEntityTest extends EntityTestBase {
@@ -119,6 +124,117 @@ public class SupplyDocumentEntityTest extends EntityTestBase {
         MetadataAttributeSharedStatesResponse response = api.entity().supply().metadata().get();
 
         assertFalse(response.getCreateShared());
+    }
+
+    @Test
+    public void newTest() throws IOException, LognexApiException {
+        LocalDateTime time = LocalDateTime.now().withNano(0);
+        SupplyDocumentEntity e = api.entity().supply().newDocument();
+
+        assertEquals("", e.getName());
+        assertTrue(e.getVatEnabled());
+        assertTrue(e.getVatIncluded());
+        assertEquals(Long.valueOf(0), e.getPayedSum());
+        assertEquals(Long.valueOf(0), e.getSum());
+        assertFalse(e.getShared());
+        assertTrue(e.getApplicable());
+        assertEquals(time, e.getMoment().withNano(0));
+
+        ListEntity<OrganizationEntity> orgList = api.entity().organization().get();
+        Optional<OrganizationEntity> orgOptional = orgList.getRows().stream().
+                min(Comparator.comparing(OrganizationEntity::getCreated));
+
+        OrganizationEntity org = null;
+        if (orgOptional.isPresent()) {
+            org = orgOptional.get();
+        } else {
+            // Должно быть первое созданное юрлицо
+            fail();
+        }
+
+        assertEquals(e.getOrganization().getMeta().getHref(), org.getMeta().getHref());
+
+        ListEntity<StoreEntity> store = api.entity().store().get(filterEq("name", "Основной склад"));
+        assertEquals(1, store.getRows().size());
+        assertEquals(e.getStore().getMeta().getHref(), store.getRows().get(0).getMeta().getHref());
+
+        ListEntity<GroupEntity> group = api.entity().group().get(search("Основной"));
+        assertEquals(1, group.getRows().size());
+        assertEquals(e.getGroup().getMeta().getHref(), group.getRows().get(0).getMeta().getHref());
+    }
+
+    @Test
+    public void newByPurchaseOrderTest() throws IOException, LognexApiException {
+        PurchaseOrderDocumentEntity purchaseOrder = new PurchaseOrderDocumentEntity();
+        purchaseOrder.setName("purchaseorder_" + randomString(3) + "_" + new Date().getTime());
+        purchaseOrder.setDescription(randomString());
+        purchaseOrder.setVatEnabled(true);
+        purchaseOrder.setVatIncluded(true);
+
+        ListEntity<OrganizationEntity> orgList = api.entity().organization().get();
+        assertNotEquals(0, orgList.getRows().size());
+        purchaseOrder.setOrganization(orgList.getRows().get(0));
+
+        CounterpartyEntity agent = new CounterpartyEntity();
+        agent.setName(randomString());
+        api.entity().counterparty().post(agent);
+        purchaseOrder.setAgent(agent);
+        purchaseOrder.setMoment(LocalDateTime.now().withNano(0));
+
+        api.entity().purchaseorder().post(purchaseOrder);
+
+        LocalDateTime time = LocalDateTime.now().withNano(0);
+        SupplyDocumentEntity e = api.entity().supply().newDocument("purchaseOrder", purchaseOrder);
+
+        assertEquals("", e.getName());
+        assertEquals(purchaseOrder.getVatEnabled(), e.getVatEnabled());
+        assertEquals(purchaseOrder.getVatIncluded(), e.getVatIncluded());
+        assertEquals(purchaseOrder.getPayedSum(), e.getPayedSum());
+        assertEquals(purchaseOrder.getSum(), e.getSum());
+        assertFalse(e.getShared());
+        assertTrue(e.getApplicable());
+        assertEquals(time, e.getMoment().withNano(0));
+        assertEquals(purchaseOrder.getMeta().getHref(), e.getPurchaseOrder().getMeta().getHref());
+        assertEquals(purchaseOrder.getAgent().getMeta().getHref(), e.getAgent().getMeta().getHref());
+        assertEquals(purchaseOrder.getGroup().getMeta().getHref(), e.getGroup().getMeta().getHref());
+        assertEquals(purchaseOrder.getOrganization().getMeta().getHref(), e.getOrganization().getMeta().getHref());
+        assertEquals(purchaseOrder.getOrganizationAccount().getMeta().getHref(), e.getOrganizationAccount().getMeta().getHref());
+    }
+
+    @Test
+    public void newByInvoicesInTest() throws IOException, LognexApiException {
+        InvoiceInDocumentEntity invoiceIn = new InvoiceInDocumentEntity();
+        invoiceIn.setName("invoiceout_" + randomString(3) + "_" + new Date().getTime());
+        invoiceIn.setVatEnabled(true);
+        invoiceIn.setVatIncluded(true);
+
+        ListEntity<OrganizationEntity> orgList = api.entity().organization().get();
+        assertNotEquals(0, orgList.getRows().size());
+        invoiceIn.setOrganization(orgList.getRows().get(0));
+
+        CounterpartyEntity agent = new CounterpartyEntity();
+        agent.setName(randomString());
+        api.entity().counterparty().post(agent);
+        invoiceIn.setAgent(agent);
+
+        api.entity().invoicein().post(invoiceIn);
+
+        LocalDateTime time = LocalDateTime.now().withNano(0);
+        SupplyDocumentEntity e = api.entity().supply().newDocument("invoicesIn", Collections.singletonList(invoiceIn));
+
+        assertEquals("", e.getName());
+        assertEquals(invoiceIn.getVatEnabled(), e.getVatEnabled());
+        assertEquals(invoiceIn.getVatIncluded(), e.getVatIncluded());
+        assertEquals(invoiceIn.getPayedSum(), e.getPayedSum());
+        assertEquals(invoiceIn.getSum(), e.getSum());
+        assertFalse(e.getShared());
+        assertTrue(e.getApplicable());
+        assertEquals(time, e.getMoment().withNano(0));
+        assertEquals(1, e.getInvoicesIn().size());
+        assertEquals(invoiceIn.getMeta().getHref(), e.getInvoicesIn().get(0).getMeta().getHref());
+        assertEquals(invoiceIn.getAgent().getMeta().getHref(), e.getAgent().getMeta().getHref());
+        assertEquals(invoiceIn.getGroup().getMeta().getHref(), e.getGroup().getMeta().getHref());
+        assertEquals(invoiceIn.getOrganization().getMeta().getHref(), e.getOrganization().getMeta().getHref());
     }
 
     private SupplyDocumentEntity createSimpleDocumentSupply() throws IOException, LognexApiException {
