4f1967831c207f2b6986a81abc8f4204b153b388
ereztsov <ereztsov@moysklad.ru>
2019-05-27 12:40:40 +0300

    MC-31037 Fixed tests Добавлена поддержка создания шаблонов по другим документам

--- src/main/java/com/lognex/api/clients/endpoints/DocumentNewEndpoint.java
+++ src/main/java/com/lognex/api/clients/endpoints/DocumentNewEndpoint.java
@@ -1,11 +1,13 @@
 package com.lognex.api.clients.endpoints;
 
+import com.lognex.api.entities.DocumentTemplateEntity;
 import com.lognex.api.entities.MetaEntity;
 import com.lognex.api.entities.documents.DocumentEntity;
 import com.lognex.api.utils.HttpRequestExecutor;
 import com.lognex.api.utils.LognexApiException;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface DocumentNewEndpoint<T extends MetaEntity> extends Endpoint {
     @ApiEndpoint
@@ -17,10 +19,28 @@ public interface DocumentNewEndpoint<T extends MetaEntity> extends Endpoint {
     }
 
     @ApiEndpoint
-    default T newDocument(DocumentEntity documentEntity) throws IOException, LognexApiException {
+    default T newDocument(DocumentTemplateEntity documentTemplateEntity) throws IOException, LognexApiException {
         return HttpRequestExecutor.
                 path(api(), path() + "new").
-                body(documentEntity).
+                body(documentTemplateEntity).
                 put((Class<T>) entityClass());
+    }
+
+    @ApiEndpoint
+    default T newDocument(String templateClassString, DocumentEntity document) throws IOException, LognexApiException {
+        DocumentTemplateEntity documentTemplateEntity = new DocumentTemplateEntity();
+        documentTemplateEntity.setDocumentType(templateClassString);
+        documentTemplateEntity.setDocument(document);
+
+        return newDocument(documentTemplateEntity);
+    }
+
+    @ApiEndpoint
+    default T newDocument(String templateClassString, List<DocumentEntity> documents) throws IOException, LognexApiException {
+        DocumentTemplateEntity documentTemplateEntity = new DocumentTemplateEntity();
+        documentTemplateEntity.setDocumentType(templateClassString);
+        documentTemplateEntity.setDocuments(documents);
+
+        return newDocument(documentTemplateEntity);
     }
 }
