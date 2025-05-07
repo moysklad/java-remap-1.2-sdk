1f97b413e75c86beeccebd895351b28ae5017e76
IvchenkoAlexandr <aleksandr.ivchenko19@gmail.com>
2025-05-07 18:41:54 +0600

    MC-83237 fake commit

--- src/main/java/ru/moysklad/remap_1_2/clients/OrganizationClient.java
+++ src/main/java/ru/moysklad/remap_1_2/clients/OrganizationClient.java
@@ -15,6 +15,8 @@ import ru.moysklad.remap_1_2.utils.params.ApiParam;
 import java.io.IOException;
 import java.util.Collection;
 
+
+
 import java.util.List;
 
 public final class OrganizationClient
