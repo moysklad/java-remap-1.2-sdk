bd4653239e11f22f49ff5ce731817dc9d94b2a6d
megorov <megorov@moysklad.ru>
2026-02-20 15:03:43 +0300

    MC-91981-test унифицирование ojectMapper в JsonUtils

--- src/main/java/ru/moysklad/remap_1_2/utils/json/JsonUtils.java
+++ src/main/java/ru/moysklad/remap_1_2/utils/json/JsonUtils.java
@@ -2,27 +2,18 @@ package ru.moysklad.remap_1_2.utils.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.NoArgsConstructor;
-import ru.moysklad.remap_1_2.entities.Meta;
+import ru.moysklad.remap_1_2.ApiClient;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public final class JsonUtils {
     public static ObjectMapper createObjectMapperWithMetaAdapter() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = ApiClient.createObjectMapper();
 
         objectMapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        SimpleModule module = new SimpleModule();
-
-        module.addSerializer(Meta.Type.class, new Meta.Type.Serializer());
-        module.addDeserializer(Meta.Type.class, new Meta.Type.Deserializer());
-
-        objectMapper.registerModule(module);
-        objectMapper.findAndRegisterModules();
 
         return objectMapper;
     }
