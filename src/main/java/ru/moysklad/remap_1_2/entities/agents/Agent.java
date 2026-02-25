8e54fe55aabfb31dd67731e812becb3a600b87ca
megorov <megorov@moysklad.ru>
2026-02-25 08:54:58 +0300

    MC-91981-test добавление не строгой десериализации 3

--- src/main/java/ru/moysklad/remap_1_2/entities/agents/Agent.java
+++ src/main/java/ru/moysklad/remap_1_2/entities/agents/Agent.java
@@ -1,5 +1,7 @@
 package ru.moysklad.remap_1_2.entities.agents;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.NoArgsConstructor;
 import ru.moysklad.remap_1_2.entities.Fetchable;
 import ru.moysklad.remap_1_2.entities.MetaEntity;
@@ -7,6 +9,16 @@ import ru.moysklad.remap_1_2.entities.MetaEntity;
 /**
  * Сущность агентов
  */
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "meta.type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Counterparty.class, name = "counterparty"),
+        @JsonSubTypes.Type(value = Employee.class, name = "employee"),
+        @JsonSubTypes.Type(value = Organization.class, name = "organization"),
+})
 @NoArgsConstructor
 public abstract class Agent extends MetaEntity implements Fetchable {
     public Agent(String id) {
